28c23a37c5e055222b4f5bf7af54010e3b0f1d6d
Giang <giang.to.7737@gmail.com>
2019-06-01 13:56:27 -0700

    setup dotenv

--- config/index.js
+++ config/index.js
@@ -1,3 +1,9 @@
 const Sequelize = require('sequelize')
 
-module.exports = new Sequelize('mysql://snud37vu71x3u1bb:xvaeugfjz5ia0kza@tyduzbv3ggpf15sx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/boqbypmqsg2r7t65')
+if (process.env.NODE_ENV === 'production') {
+    sqlstr = process.env.JAWSDB_URL
+} else {
+    sqlstr = process.env.LOCALDB
+}
+
+module.exports = new Sequelize(sqlstr)
