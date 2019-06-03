21b546f81c9f8675461aa7a11f96c0804048a250
Kathy Chiem <kchie002@gmail.com>
2019-06-02 18:48:50 -0700

    fixed image errors in login page dropdown

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -4,84 +4,106 @@ import 'semantic-ui-css/semantic.min.css'
 import './Login.css'
 import { withRouter } from 'react-router-dom';
 
+//Import all character icons
+import AryaImage from '../media/profile-images/arya-stark.png'
+import BenjenImage from '../media/profile-images/benjen-stark.png'
+import CatelynImage from '../media/profile-images/catelyn-stark.png'
+import CerseiImage from '../media/profile-images/cersei-lannister.png'
+import DanyImage from '../media/profile-images/dany.png'
+import JaimeImage from '../media/profile-images/jaime-lannister.png'
+import JonImage from '../media/profile-images/jon-snow.png'
+import MelisandreImage from '../media/profile-images/melisandre.png'
+import NedImage from '../media/profile-images/ned-stark.png'
+import OberynImage from '../media/profile-images/oberyn-martell.png'
+import RobImage from '../media/profile-images/rob-stark.png'
+import RobertImage from '../media/profile-images/robert-baratheon.png'
+import SansaImage from '../media/profile-images/sansa-stark.png'
+import TyrionImage from '../media/profile-images/tyrion-lannister.png'
+
 const faceOptions = [
   {
     key: 'Arya Stark',
     text: 'Arya Stark',
     value: 'Arya Stark',
-    image: { avatar: true, src: '../media/profile-images/arya-stark.jpg' },
+    image: { avatar: true, src: AryaImage },
   },
   {
     key: 'Benjen Stark',
     text: 'Benjen Stark',
     value: 'Benjen Stark',
-    image: { avatar: true, src: '../media/profile-images/benjen-stark.png' },
+    image: { avatar: true, src: BenjenImage },
   },
   {
     key: 'Catelyn Stark',
     text: 'Catelyn Stark',
     value: 'Catelyn Stark',
-    image: { avatar: true, src: '../media/profile-images/catelyn-stark.png' },
+    image: { avatar: true, src: CatelynImage },
   },
   {
     key: 'Cersei Lannister',
     text: 'Cersei Lannister',
     value: 'Cersei Lannister',
-    image: { avatar: true, src: '../media/profile-images/cersei-lannister.png' },
+    image: { avatar: true, src: CerseiImage },
   },
   {
     key: 'Daenerys Targaryen',
     text: 'Daenerys Targaryen',
     value: 'Daenerys Targaryen',
-    image: { avatar: true, src: '../media/profile-images/dany.png' },
+    image: { avatar: true, src: DanyImage },
   },
   {
     key: 'Jaime Lannister',
     text: 'Jaime Lannister',
     value: 'Jaime Lannister',
-    image: { avatar: true, src: '../media/profile-images/jaime-lannister.png' },
+    image: { avatar: true, src: JaimeImage },
   },
   {
     key: 'Jon Snow',
     text: 'Jon Snow',
     value: 'Jon Snow',
-    image: { avatar: true, src: '../media/profile-images/jon-snow.png' },
+    image: { avatar: true, src: JonImage },
   },
   {
     key: 'Melisandre',
     text: 'Melisandre',
     value: 'Melisandre',
-    image: { avatar: true, src: '../media/profile-images/melisandre.png' },
+    image: { avatar: true, src: MelisandreImage },
   },
   {
     key: 'Ned Stark',
     text: 'Ned Stark',
     value: 'Ned Stark',
-    image: { avatar: true, src: '../media/profile-images/ned-stark.png' },
+    image: { avatar: true, src: NedImage },
   },
   {
     key: 'Oberyn Martell',
     text: 'Oberyn Martell',
     value: 'Oberyn Martell',
-    image: { avatar: true, src: '../media/profile-images/oberyn-martell.png' },
+    image: { avatar: true, src: OberynImage },
+  },
+  {
+    key: 'Rob Stark',
+    text: 'Rob Stark',
+    value: 'Rob Stark',
+    image: { avatar: true, src: RobImage },
   },
   {
     key: 'Robert Baratheon',
     text: 'Robert Baratheon',
     value: 'Robert Baratheon',
-    image: { avatar: true, src: '../media/profile-images/robert-baratheon.png' },
+    image: { avatar: true, src: RobertImage },
   },
   {
     key: 'Sansa Stark',
     text: 'Sansa Stark',
     value: 'Sansa Stark',
-    image: { avatar: true, src: '../media/profile-images/sansa-stark.png' },
+    image: { avatar: true, src: SansaImage },
   },
   {
     key: 'Tyrion Lannister',
     text: 'Tyrion Lannister',
     value: 'Tyrion Lannister',
-    image: { avatar: true, src: '../media/profile-images/tyrion-lannister.png' },
+    image: { avatar: true, src: TyrionImage },
   },
 ]
 
