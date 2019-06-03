e375cdf6bf283a16bea193ae1048d7a07ec6e2c3
Erica P <erica.parra@rocketmail.com>
2019-06-03 13:18:51 -0700

    Added delete functionality to kill list /cleaned out misc styling

--- client/src/components/ToDoList/MainTodo.js
+++ client/src/components/ToDoList/MainTodo.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Todo from './Todo'
 import Todolist from './Todolist';
 import './MainToDo.css'
-import { Segment , Grid , Divider , Icon} from 'semantic-ui-react'
+import { Segment , Grid , Divider , Icon , Button} from 'semantic-ui-react'
 
 
 import uuid from "uuid";
@@ -18,6 +18,25 @@ class MainTodo extends Component {
       name:e.target.value
     });
   };
+
+  handleClear = () => {this.setState({names:[],
+    id:"",
+    name:"",
+    editName:false})
+  console.log("Handle clear working@!")
+  } 
+
+  handleDelete = (id) =>{
+    console.log(`deleting ${id}`)
+    let nameArr = this.state.names;
+    for (let i = 0; i<nameArr.length; i++){
+      if (nameArr[i].id === id){
+        nameArr.splice(i,1)
+      }
+    }
+    this.setState({names: nameArr})
+  }
+
   handleSubmit = (e)=> {
     e.preventDefault();
 
@@ -46,13 +65,19 @@ console.log(newName);
 
         <Segment id="segmentBody">
           <Grid columns={2}  >
-            <Grid.Column floated='left' width={8}>
+            <Grid.Column textAlign="center">
             <h3 id="addFaceHeader"> Faces To Collect </h3>
             <Todo name={this.state.name} handleChange=
             {this.handleChange} handleSubmit = {this.handleSubmit}/>
-            </Grid.Column>
-            <Grid.Column floated='right' width={8}>
-            <Todolist names={this.state.names}/>
+            </Grid.Column >
+            <Grid.Column textAlign="center">
+            <Todolist names={this.state.names} HandleDelete={this.handleDelete}/>
+            <Button  type="button"
+                className="clearBtn" type="submit" color="black" size="medium" basic inverted 
+                onClick={this.handleClear}
+                >
+                Collected
+                </Button>
             </Grid.Column>
           </Grid>
 
--- client/src/components/ToDoList/Todolist.js
+++ client/src/components/ToDoList/Todolist.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react'
 import Todoitem from './Todoitem';
 import './MainToDo.css'
-import { Button , List , Grid, Icon, Header } from 'semantic-ui-react'
+import { Checkbox , List , Grid, Container, Header, ListContent, GridColumn } from 'semantic-ui-react'
 
 export default class Todolist extends Component {
+    deleteName = (e) => {
+        e.preventDefault()
+        console.log(e.target.id)
+        this.props.HandleDelete(e.target.id)
+    }
+
     render() {
         const {names}=this.props
         return (
@@ -15,18 +21,28 @@ export default class Todolist extends Component {
                     </Header>
                 </Grid.Column>
                  <Grid.Column>
-                    <List>
-                        <List.Item id="listItem">
+                    <List divided >
+                        <List.Item id="listItem" textAlign="center">
                     {
                         names.map(name => {
                             return(
+                            <Grid>   
+                             <Grid.Column floated='left' width={13}>
+                                
                             <Todoitem key={name.id} title={name.title}/>
+                            
+                         </Grid.Column>
+                         <Grid.Column floated='left'>
+                            <Checkbox id={name.id} onClick={this.deleteName}/>
+                        </Grid.Column>
+                          </Grid> 
                         )
                     })
             }
                         </List.Item>
                     </List>
-                        <Button color="black" basic inverted id="clearBtn" size="medium">Collected</Button>
+                    
+                        
                     
                 </Grid.Column>
             </Grid>
